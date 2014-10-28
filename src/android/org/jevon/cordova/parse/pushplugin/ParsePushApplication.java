c89ec93a3c1cf49e94ff19e84b35b39a820d062d
Jevon Wright <jevon@jevon.org>
2014-10-29 12:00:48 +1300

    Adding logging statements

--- src/android/org/jevon/cordova/parse/pushplugin/ParsePushApplication.java
+++ src/android/org/jevon/cordova/parse/pushplugin/ParsePushApplication.java
@@ -5,24 +5,27 @@ import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.util.Log;
 
 import com.parse.Parse;
 
 public class ParsePushApplication extends Application {
 	@Override
 	public void onCreate() {
+		Log.i("ParsePushApplication", "onCreate");
 		super.onCreate();
 		
 		Context context = this.getApplicationContext();
 		ApplicationInfo ai = null;
 		try {
-			ai = context.getPackageManager().getApplicationInfo(context.getPackageName(),PackageManager.GET_META_DATA);
+			ai = context.getPackageManager().getApplicationInfo(context.getPackageName(), PackageManager.GET_META_DATA);
 		} catch (NameNotFoundException e) {
 			throw new RuntimeException(e);
 		}
 		
 		String appId = (String) ai.metaData.get("ParseAppId");
 		String clientSecret = (String) ai.metaData.get("ParseClientSecret");
+		Log.i("ParsePushApplication", "Initialising Parse SDK with '" + appId + "', '" + clientSecret + "'");
 		
 		if (appId == null || appId.isEmpty()) {
 			throw new IllegalArgumentException("Parse AppId needs to be defined");
@@ -32,6 +35,9 @@ public class ParsePushApplication extends Application {
 		}
 		
 		// initialize the Parse SDK
+		Log.i("ParsePushApplication", "About to initialise Parse SDK");
 		Parse.initialize(this, appId, clientSecret);
+		Log.i("ParsePushApplication", "Parse SDK initialised");
+		
 	}
 }
